f4ea6bda1e080a25a3ad329d6736f4bfd6a08c5b
Pedro <pedro.s.morais@gmail.com>
2024-08-06 12:32:43 -0300

    novas cores

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const getTime = async () => {
     .then((response) => response.json())
     .then((data) => {
       const currentTime = data.datetime;
-      const promoStartTime = "2024-06-07T09:00:00.000000-03:00";
-      const promoEndTime = "2024-06-07T12:00:00.000000-03:00";
+      const promoStartTime = "2024-01-07T10:00:00.000000-03:00";
+      const promoEndTime = "2024-08-07T12:00:00.000000-03:00";
 
       if (promoStartTime <= currentTime && promoEndTime >= currentTime) {
         mountElmentBlock();
@@ -17,24 +17,24 @@ const getTime = async () => {
 };
 
 const mountElmentBlock = () => {
-  const hasKeyOnLocalStorage = localStorage.getItem("gf24-junho");
+  const hasKeyOnLocalStorage = localStorage.getItem("gf24-cores");
 
   if (hasKeyOnLocalStorage === "true") return;
 
   const body = document.querySelector("body");
 
-  body.classList.add("gf24-junho");
+  body.classList.add("gf24-cores");
 
   const div = document.createElement("div");
   div.setAttribute("class", "giraFriday_overlay");
   div.innerHTML = `
     <div class="giraFriday_modal slide-top">
       <div class="giraFriday_modal-logo">
-        <img src="https://51734.cdn.simplo7.net/static/51734/galeria/171770668324671.png" />
+        <img src="/images/cores_logos.webp" />
       </div>
-      <p class="giraFriday_modal-text">Drop 2 - Coleção aconchego está disponível!</p>
-      <p class="giraFriday_modal-subtext desktop">Acesso Exclusivo para você - Das 9h às 12h</p>
-      <p class="giraFriday_modal-subtext mobile">Acesso Exclusivo para você <br> Das 9h às 12h</p>
+      <p class="giraFriday_modal-text">4 Novas cores do nosos vestido amplo já disponíveis!</p>
+      <p class="giraFriday_modal-subtext desktop">Acesso Exclusivo para você - Das 10h às 12h</p>
+      <p class="giraFriday_modal-subtext mobile">Acesso Exclusivo para você <br> Das 10h às 12h</p>
       <form>
         <input type="password" placeholder="Senha" />
         <button type="submit">Entrar</button>
@@ -48,9 +48,9 @@ const mountElmentBlock = () => {
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (input.value.toLowerCase() === "aproveita") {
-      localStorage.setItem("gf24-junho", true);
-      body.classList.remove("gf24-junho");
+    if (input.value.toLowerCase() === "novas cores") {
+      localStorage.setItem("gf24-cores", true);
+      body.classList.remove("gf24-cores");
       const element = document.querySelector(".giraFriday_overlay");
       setTimeout(() => {
         element.remove();
