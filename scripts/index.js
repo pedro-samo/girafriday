05a206675525b19fd058f469062d092668cae418
Pedro <pedro.s.morais@gmail.com>
2025-12-10 16:58:14 -0300

    fix: promo end time

--- scripts/index.js
+++ scripts/index.js
@@ -85,7 +85,7 @@ const checkClubeGiraQueryParam = () => {
   const currentTime = new Date();
   const promoEndTime = new Date("2025-12-14T23:59:00.000000-03:00");
 
-  if (hasShowLinkOnLocalStorage && currentTime <= promoEndTime) {
+  if (hasShowLinkOnLocalStorage && currentTime >= promoEndTime) {
     return localStorage.removeItem("clubeGira2025Link");
   }
 
