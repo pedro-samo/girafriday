d1029b36029482493bea7ff2e51c4875ccc38c4d
Pedro <pedro.s.morais@gmail.com>
2025-12-09 22:10:18 -0300

    feat: link menu

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ const mountElmentBlock = () => {
   if (hasKeyOnLocalStorage === "true") return;
 
   const body = document.querySelector("body");
-``
+  ``;
   body.classList.add("gf-encerramento");
 
   const div = document.createElement("div");
@@ -31,14 +31,18 @@ const mountElmentBlock = () => {
       <form>
     </div>
   `;
-  body.appendChild(div);1
+  body.appendChild(div);
+  1;
 
   const form = document.querySelector(".giraFriday_modal form");
   const input = document.querySelector(".giraFriday_modal input");
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (input.value.toLowerCase() === "ultimogiro" || input.value.toLowerCase() === "últimogiro") {
+    if (
+      input.value.toLowerCase() === "ultimogiro" ||
+      input.value.toLowerCase() === "últimogiro"
+    ) {
       localStorage.setItem("gf-encerramento", true);
       body.classList.remove("gf-encerramento");
       const element = document.querySelector(".giraFriday_overlay");
@@ -60,10 +64,47 @@ const mountElmentBlock = () => {
   });
 };
 
+const checkClubeGiraQueryParam = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const clubeGiraParam = urlParams.get("clubeGira2025");
+
+  if (clubeGiraParam === "true") {
+    localStorage.setItem("clubeGira2025Link", "true");
+  }
+};
+
+const createLinkGira = () => {
+  const desktopMenu = document.querySelector(".js-desktop-nav.desktop-nav");
+  const mobileMenu = document.querySelector("#nav-hamburger .clear-both ul");
+
+  const hasShowLinkOnLocalStorage = localStorage.getItem("clubeGira2025Link");
+
+  if (desktopMenu && hasShowLinkOnLocalStorage === "true") {
+    const elementDesktop = `<li class="desktop-nav-item ">
+		      <a href="https://www.aaventurina.com.br/clube-gira" class="desktop-nav-link">
+          <span class="divider-bullet opacity-50 m-right-half" aria-hidden="true"></span>
+						Clube Gira
+		      </a>
+			</li>`;
+    const secondChild = desktopMenu.children[1];
+    if (secondChild) {
+      secondChild.insertAdjacentHTML("beforebegin", elementDesktop);
+    } else {
+      desktopMenu.innerHTML += elementDesktop;
+    }
+  }
+
+  if (mobileMenu && hasShowLinkOnLocalStorage === "true") {
+    const elementMobile = `<li>
+                <a class="hamburger-panel-link" href="https://www.aaventurina.com.br/clube-gira">
+                    Clube Gira
+                </a>
+             </li>`;
+    mobileMenu.prepend(elementMobile);
+  }
+};
+
 window.addEventListener("DOMContentLoaded", () => {
-  // const IS_PRODUCTION = window.location.hostname === "www.giraflorstore.com.br";
-  // if (IS_PRODUCTION) return;
-  // getTime();
-  // changeGoBackButton();
-  console.log("Script loaded");
+  checkClubeGiraQueryParam();
+  createLinkGira();
 });
